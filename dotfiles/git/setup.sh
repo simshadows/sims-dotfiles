acaf89417c4a7ac7e37eb7ed7d226a311d3c39ee
simshadows <contact@simshadows.com>
2018-04-20 00:14:26 +1000

    Git global config is now a full .gitconfig file.
    Previously, I used the 'git config --global' command. This change should hopefully allow me to keep better track of my configuration.

--- dotfiles/git/setup.sh
+++ dotfiles/git/setup.sh
@@ -10,13 +10,8 @@ set -e
 src_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 echo "Setting up symlinks..."
+ln -sfT "$src_dir/.gitconfig" ~/.gitconfig
 ln -sfT "$src_dir/.gitignore_global" ~/.gitignore_global
-
-echo "Running git config commands..."
-git config --global user.email 'contact@simshadows.com'
-git config --global user.name 'simshadows'
-git config --global core.editor vim
-git config --global core.excludesfile ~/.gitignore_global
 
 echo "DONE!"
 printf "\n"
