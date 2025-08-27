cd5274eb243a481b71901be0ddb722d87e41be24
simshadows <contact@simshadows.com>
2025-08-28 00:14:53 +1000

    Added VSCode snippets

--- dotfiles/neovim/.config/nvim/init.lua
+++ dotfiles/neovim/.config/nvim/init.lua
@@ -245,7 +245,9 @@ require("lazy").setup({
     -- colorscheme that will be used when installing plugins.
     install = { colorscheme = { "habamax" } },
     -- automatically check for plugin updates
-    checker = { enabled = true },
+    checker = {
+        enabled = false
+    },
 })
 
 ----------------------------------------------------------------------
