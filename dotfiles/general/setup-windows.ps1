22971227855cd8b3f8682abb2c796eae9b4d1c61
simshadows <contact@simshadows.com>
2025-10-31 17:23:44 +1100

    Update Windows setup script for Claude Code

--- dotfiles/general/setup-windows.ps1
+++ dotfiles/general/setup-windows.ps1
@@ -5,6 +5,13 @@ echo "=== General ==="
 
 echo "Setting up symlinks..."
 
+New-Item -Force -ItemType SymbolicLink `
+    -Path $env:USERPROFILE\.claude\CLAUDE.md `
+    -Value $PSScriptRoot\.claude\CLAUDE.md
+New-Item -Force -ItemType SymbolicLink `
+    -Path $env:USERPROFILE\.claude\settings.json `
+    -Value $PSScriptRoot\.claude\settings.json
+
 cp $PSScriptRoot\.config\vlc\vlcrc $env:USERPROFILE\AppData\Roaming\vlc\vlcrc
 # We should symlink the file, but VLC seems to delete the symlink and replace it every time it updates the settings.
 #New-Item -Force -Path $env:USERPROFILE\AppData\Roaming\vlc\vlcrc -ItemType SymbolicLink -Value $PSScriptRoot\.config\vlc\vlcrc
